f1beb0fd3e88ec0415bc4759e69885754b6cd0f5
Veera Gopalakrishnan <veeraragavan.gopalakrishnan@ucsf.edu>
2015-08-12 14:18:04 -0700

    Test retrieveFlights returns application/json in header

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,6 +3,7 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 // Test snippet: http://bit.ly/1Uz2vEN
@@ -13,6 +14,13 @@ public class HelloMavenTest extends FunctionalTestCase {
 	@Test
 	public void sayHelloReturnsHelloMaven() throws Exception {
 		runFlowAndExpect("mavenFlow", "Hello Maven");
+	}
+	
+	@Test
+	public void retrieveFlightsJSON() throws Exception {
+		MuleEvent event = runFlow("retrieveFLights");
+		String contentType = event.getMessage().getOutboundProperty("Content-Type");
+		assertEquals("application/json",contentType);
 	}
 	
 	@Override
